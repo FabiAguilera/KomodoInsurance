74f02e3422f3a53b8fa20490e338057c291aafd8
Fabiola Aguilera <fabiaguilera96@gmail.com>
2021-12-22 17:39:18 -0500

    First Commit

---
+++ KomodoTest1/DevTests.cs
@@ -0,0 +1,58 @@
+﻿using KomodoInsurance.POCO;
+using KomodoInsurance01_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace KomodoTest1
+{
+    
+    [TestClass] // anotation
+    public class DevTests
+    {
+        Employee _employee = new Employee();
+        EmployeeRepo _employeeRepo = new EmployeeRepo();
+        [TestMethod] // anotation ----- we know this because they are in square brackets
+        public void CreateEmployee_ValidEmployee_ReturnsTrue() // all test methods will be void because it will not return anyhting, it is just a check
+        {
+            //Arrange - organizing/setting test method to call upon method
+
+            _employee.Id = 0;
+
+            //Act
+            bool employeeCreated = _employeeRepo.CreateEmployee(_employee);
+
+            //Assert
+            Assert.IsTrue(employeeCreated);
+
+        }
+
+        public void GetEmployees()
+        {
+           
+        }
+
+        public void GetEmployeeById()
+        {
+
+        }
+
+        public void UpdateEmployee_ValidInfo_ReturnsTrue()
+        {
+            // arrange
+            
+            _employee.newEmployeeData = 0;
+
+            // act
+            bool employeeUpdated = _employeeRepo.UpdateEmployee(_employee);
+            // assert
+            Assert.IsTrue(employeeUpdated);
+        }
+
+        public void DeleteEmployee()
+        {
+
+        }
+
+
+    }
+}
---
+++ KomodoTest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KomodoTest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("KomodoTest1")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f7e43b7b-0eaa-451f-bf4a-a24734693210")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
